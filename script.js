fe729ac17194a6cad418c50812413d41175b3990
reyzmerly <reyzmerly9@gmail.com>
2025-07-30 18:12:22 +0200

    updated now

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navArrows = document.querySelectorAll('.nav-arrow');
     const counter = document.querySelector('.portfolio-counter .current');
     let currentProject = 1;
-    const totalProjects = 6;
+    const totalProjects = 5;
 
     navArrows.forEach((arrow, index) => {
         arrow.addEventListener('click', function() {
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const counter = document.querySelector('.portfolio-counter .current');
         const progressBar = document.querySelector('.portfolio-progress-bar');
         let currentProject = 1;
-        const totalProjects = 6;
+        const totalProjects = 5;
     
         function updateProgress() {
             counter.textContent = currentProject.toString().padStart(2, '0');
@@ -67,8 +67,7 @@ const projects = [
   "Project 2", 
   "Project 3",
   "Project 4",
-  "Project 5",
-  "Project 6"
+  "Project 5"
 ];
 
 let currentIndex = 0;
@@ -94,12 +93,16 @@ function updatePortfolio() {
       projectLabel.href = 'projects.html#kora';
       projectLabel.style.pointerEvents = 'auto';
     } else if (currentIndex === 2) {
-      projectLabel.textContent = 'TiGo';
+      projectLabel.textContent = 'KoraGo';
       projectLabel.href = 'projects.html#tigo';
       projectLabel.style.pointerEvents = 'auto';
     } else if (currentIndex === 3) {
       projectLabel.textContent = 'Vistopedia';
       projectLabel.href = 'projects.html#vistopedia';
+      projectLabel.style.pointerEvents = 'auto';
+    } else if (currentIndex === 4) {
+      projectLabel.textContent = 'KoraChat';
+      projectLabel.href = 'projects.html#korachat';
       projectLabel.style.pointerEvents = 'auto';
     } else {
       projectLabel.textContent = '';
